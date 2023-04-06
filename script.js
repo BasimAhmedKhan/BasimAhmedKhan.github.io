d7303e212b52b8fd88c93cd0b12a98383107125d
Basim Khan <kbasim039@gmail.com>
2023-04-06 18:10:11 +0500

    fix: typewriter effect.

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 // Typewriter Effect.
 
 var typeName = document.getElementById("name");
-var myName = ["Basim Ahmed Khan", "A Frontend Developer", "A Web Designer", "A Game Developer"];
+var myName = ["Basim Ahmed Khan", "A Frontend Developer", "A Web Designer", "A Freelancer", "An Automation Developer"];
 let charPosition = 0;
 let textPosition = 0;
 
-let typeWriter = setInterval(() => {
-    let span = document.createElement("span");
+typeWriter = () => {
+    typeName.innerText = myName[textPosition].substring(0, charPosition);
+    if(charPosition++ != myName[textPosition].length)
+    {
+        setTimeout(typeWriter, 100);
+    }
     let text = myName[textPosition];
-    span.innerText = text[charPosition];
-    typeName.appendChild(span);
-    if(++charPosition === text.length)
+    if(charPosition === text.length+1)
     {
-        textPosition++;
-        let delChild = setInterval(() => {
-            typeName.removeChild(typeName.lastElementChild);
-            if(--charPosition === 0)
-            {
-                clearInterval(delChild);
-            }
-        }, 100);
+        setTimeout(delChar, 3000);
     }
-    if(textPosition === myName.length)
+};
+
+delChar = () => {
+    typeName.innerText = myName[textPosition].substring(0, charPosition);
+    if(--charPosition >= 0)
     {
-        clearInterval(typeWriter);
+        setTimeout(delChar, 50);
     }
-}, 200);
+    if(charPosition === 0)
+    {
+        if(textPosition === myName.length-1)
+        {
+            textPosition = 0;
+        }
+        else
+        {
+            textPosition++;
+        }
+        setTimeout(typeWriter, 1000);
+    }
+};
+
+typeWriter();
 
 
 // Hamburger Nav.
